504f3e6eaface988c31c2ea7b799babcd23b18db
dante <dante@DESKTOP-JPIJ01E>
2022-03-26 20:17:57 +0300

    Добавьте файлы проекта.

---
+++ 26.03.22/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			NumberFormatInfo nfi = new NumberFormatInfo()
+			{
+				NumberDecimalSeparator = ".",
+			};
+
+			int k = 0;
+			double a, b, res;
+			char c;
+			string str1, str2;
+
+			while (k != 1)
+			{
+				Console.WriteLine("Введите первое число...");
+				str1 = Console.ReadLine();
+				bool tryParse1 = double.TryParse(str1, out a);
+				Console.WriteLine("Введите второе число...");
+				str2 = Console.ReadLine();
+				bool tryParse2 = double.TryParse(str2, out b);
+
+				if (tryParse1 && tryParse2)
+				{
+					Console.WriteLine("Введите знак: '+' '-' '*' '/'");
+					c = Convert.ToChar(Console.ReadLine());
+					switch (c)
+					{
+						case '+':
+							res = a + b;
+							Console.WriteLine($"{a} + {b} = {res}");
+							break;
+						case '-':
+							res = a - b;
+							Console.WriteLine($"{a} - {b} = {res}");
+							break;
+						case '*':
+							res = a * b;
+							Console.WriteLine($"{a} * {b} = {res}");
+							break;
+						case '/':
+							res = a / b;
+							Console.WriteLine($"{a} / {b} = {res}");
+							break;
+					}
+					k++;
+				}
+				else
+				{
+					try
+					{
+						a = double.Parse(str1, nfi);
+						b = double.Parse(str2, nfi);
+						Console.WriteLine("Введите знак: '+' '-' '*' '/'");
+						c = Convert.ToChar(Console.ReadLine());
+						switch (c)
+						{
+							case '+':
+								res = a + b;
+								Console.WriteLine($"{a} + {b} = {res}");
+								break;
+							case '-':
+								res = a - b;
+								Console.WriteLine($"{a} - {b} = {res}");
+								break;
+							case '*':
+								res = a * b;
+								Console.WriteLine($"{a} * {b} = {res}");
+								break;
+							case '/':
+								res = a / b;
+								Console.WriteLine($"{a} / {b} = {res}");
+								break;
+						}
+						k++;
+					}
+					catch
+					{
+						Console.WriteLine("Ошибка! Вы ввели неверные данные.");
+					}
+				}
+			}
+		}
+	}
+}
